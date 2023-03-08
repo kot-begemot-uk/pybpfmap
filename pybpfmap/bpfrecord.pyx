81c5bb9a434f92a9da07757c6d0d032dfdf82d99
Anton Ivanov <anton.ivanov@cambridgegreys.com>
2023-03-08 15:06:12 +0000

    Update licenses
    Signed-off-by: Anton Ivanov <anton.ivanov@cambridgegreys.com>

--- pybpfmap/bpfrecord.pyx
+++ pybpfmap/bpfrecord.pyx
@@ -1,5 +1,14 @@
 '''Python presentation of BPF map records'''
 
+# pybpfmap, Copyright (c) 2023 RedHat Inc
+# pybpfmap, Copyright (c) 2023 Cambridge Greys Ltd
+
+# This source code is licensed under both the BSD-style license (found in the
+# LICENSE file in the root directory of this source tree) and the GPLv2 (found
+# in the COPYING file in the root directory of this source tree).
+# You may select, at your option, one of the above-listed licenses.
+
+
 import struct
 import sys
 import cython
--- pybpfmap/tests/test_encode_decode.py
+++ pybpfmap/tests/test_encode_decode.py
@@ -4,6 +4,15 @@
 '''BPF Map parser test
 '''
 
+# pybpfmap, Copyright (c) 2023 RedHat Inc
+# pybpfmap, Copyright (c) 2023 Cambridge Greys Ltd
+
+# This source code is licensed under both the BSD-style license (found in the
+# LICENSE file in the root directory of this source tree) and the GPLv2 (found
+# in the COPYING file in the root directory of this source tree).
+# You may select, at your option, one of the above-listed licenses.
+
+
 from pybpfmap.bpfrecord import BPFRecord
 
 from nose.tools import ok_ as assert_
